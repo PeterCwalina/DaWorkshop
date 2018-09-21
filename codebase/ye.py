09194f2831cd07b167e64e76149bcc741d0c178a
thegamer5100 <pcwalina@stuy.edu>
2018-09-21 13:17:46 -0400

    stuffs

--- codebase/ye.py
+++ codebase/ye.py
@@ -1,4 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
+coll = [0,1,1,2,3,5,8]
+@app.route("/")
+def home():
+    return render_template(
+        'lists.html',
+        foo = "foooo",
+        collection = coll
+        )
+                               
+                
+app.debug = True
 app.run()
