605915fd5b767bd9e75e23abea1a72de2d89c149
Peter <pcwalina@stuy.edu>
2018-09-25 19:31:59 -0400

    extra stuff

--- 10_occupy_flask_st/ye.py
+++ 10_occupy_flask_st/ye.py
@@ -8,13 +8,18 @@
 from util import csvStuff
 from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return '<a href="/occupations">To the homework</a>'
+
 @app.route("/occupations")
 def occRoute():
-    occDict = csvStuff.makeDict('data/occupations.csv')    
+    occDict = csvStuff.makeDict('data/occupations.csv') #creates the dictionary of occupations
     return render_template('occ.html',#rendering the template with set params
                            title ='Occupation Data',
                            heading = 'Here\'s some cool data on occupation percentages',
-                           occ = csvStuff.randomKey(occDict),
+                           occ = csvStuff.randomKey(occDict), #sets the variable occ to a random occupation
                            dict = occDict
     )
 
