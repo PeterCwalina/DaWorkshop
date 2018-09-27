20da1921ce6ac6187da62e21f19d1c14db340367
thegamer5100 <pcwalina@stuy.edu>
2018-09-27 12:49:57 -0400

    starter changed

--- codebase/ye.py
+++ codebase/ye.py
@@ -1,4 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/")
+def nothing():
+    return 'public/idontexist.html'
+
+@app.route("/o")
+def noting():
+    x = 2
+    while x > 1:
+        x +=1
+
+        
+    return 'public/yes.html'
+
+
+
 app.run()
+app.debug = True
